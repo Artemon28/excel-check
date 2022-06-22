b9cad1d52076b29c73a7fef43b659b73683402dd
Artemiy <los28.2001@mail.ru>
2022-06-22 11:25:12 +0300

    Reading by rows

--- excel-check/src/FileHandler/DowntimesReader.java
+++ excel-check/src/FileHandler/DowntimesReader.java
@@ -3,10 +3,12 @@ package FileHandler;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashSet;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -27,45 +29,57 @@ public class DowntimesReader implements ExcelReader{
 		
 		FileInputStream file = new FileInputStream(excelFile);
 		Workbook workbook = new XSSFWorkbook(file);
-		Sheet sheet = workbook.getSheetAt(2);
-		
-		int rowNum = sheet.getLastRowNum();
-		int columnNum = sheet.getRow(0).getPhysicalNumberOfCells();
-		
-		for (int i = 0; i < columnNum; i++) {
-			StringBuilder strColumn = new StringBuilder();
-			Cell headCell = sheet.getRow(0).getCell(0);
-			CellStyle headStyle = headCell.getCellStyle();
-			short blueColorIndex = 64;
-			if (headStyle.getFillBackgroundColor() != blueColorIndex)
+		for (int k = 1; k < workbook.getNumberOfSheets(); k++) {
+			Sheet sheet = workbook.getSheetAt(k);
+			HashSet<Integer> blueColumns = new HashSet<>();
+			Row headRow = sheet.getRow(0);
+			if (headRow == null)
 				continue;
-			
-			for (int j = 0; j <= rowNum; j++){
-				Cell cell = sheet.getRow(j).getCell(i);
-				switch (cell.getCellType()) {
-	            case STRING:
-	            	strColumn.append(cell.getRichStringCellValue().getString());
-	            	CellCheck check = new StringCellCheck();
-	            	check.checkCell(cell);
-	            	strColumn.append(';');
-	            	break;
-	            case NUMERIC:
-	            	if (DateUtil.isCellDateFormatted(cell)) {
-	            	    cell.getDateCellValue();
-	            	} else {
-	            	    cell.getNumericCellValue();
-	            	}
-	            	break;
-	            case BOOLEAN:
-	            	cell.getBooleanCellValue();
-	            	break;
-	            case FORMULA:
-	            	cell.getCellFormula();
-	            	break;
-				}
+			for (Cell cell : headRow) {
+				CellStyle headStyle = cell.getCellStyle();
+				if (headStyle.getFillForegroundColorColor() == null)
+					break;
+				if (headStyle.getFillForegroundColorColor().toString().equals("org.apache.poi.xssf.usermodel.XSSFColor@c6292f46"))
+					blueColumns.add(cell.getColumnIndex());
 			}
-			EZEnvironment.displayInfoMessage(EZEnvironment.getParentFrame(), strColumn.toString());
+			for (Row row : sheet) {
+				if (row.getRowNum() == 0)
+					continue;
+				StringBuilder strRow = new StringBuilder();
+			    for (Cell cell : row) {
+					if (!blueColumns.contains(cell.getColumnIndex()))
+						continue;
+					if (cell == null)
+						continue;
+					switch (cell.getCellType()) {
+		            case STRING:
+		            	CellCheck check = new StringCellCheck();
+		            	if(!check.checkCell(cell))
+		            		continue;
+		            	strRow.append(cell.getRichStringCellValue().getString());
+		            	strRow.append(';');
+		            	break;
+		            case NUMERIC:
+		            	if (DateUtil.isCellDateFormatted(cell)) {
+		            		strRow.append(cell.getDateCellValue());
+			            	strRow.append(';');
+		            	} else {
+		            		strRow.append(cell.getNumericCellValue());
+			            	strRow.append(';');
+		            	}
+		            	break;
+		            case BOOLEAN:
+		            	cell.getBooleanCellValue();
+		            	break;
+		            case FORMULA:
+		            	cell.getCellFormula();
+		            	break;
+					}
+				}
+			    if (!strRow.isEmpty())
+			    	EZEnvironment.displayInfoMessage(EZEnvironment.getParentFrame(), strRow.toString());
+			}
+			workbook.close();
 		}
-		workbook.close();
 	}
 }
